7b797651b164bc6f3464ec6494114046c6febc64
Floris Jolink <floris.jolink@gmail.com>
2018-06-07 21:25:46 +0200

    init commit

---
+++ src/main/java/me/theblockbender/voxelshortcuts/Main.java
@@ -0,0 +1,9 @@
+package me.theblockbender.voxelshortcuts;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+    public void onEnable(){
+        // template
+    }
+}
