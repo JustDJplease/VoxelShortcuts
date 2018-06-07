19e8ee3b73e9ef935963516dcb2f58e4c43e510a
Floris Jolink <floris.jolink@gmail.com>
2018-06-07 21:39:58 +0200

    added listener

--- src/main/java/me/theblockbender/voxelshortcuts/Main.java
+++ src/main/java/me/theblockbender/voxelshortcuts/Main.java
@@ -1,9 +1,10 @@
 package me.theblockbender.voxelshortcuts;
 
+import me.theblockbender.voxelshortcuts.listeners.ItemDropListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin {
     public void onEnable(){
-        // template
+        getServer().getPluginManager().registerEvents(new ItemDropListener(), this);
     }
 }
